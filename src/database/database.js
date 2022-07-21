e78f075acbab5be7d6f0af5251de1614e80d1fc8
jesm-erick <jhosucasaire@upeu.edu.pe>
2022-07-21 18:56:26 -0500

    docker

--- src/database/database.js
+++ src/database/database.js
@@ -2,18 +2,13 @@ import Sequelize from "sequelize";
 import  "dotenv/config";
 
 export const sequelize = new Sequelize(
-    'd9evgdmq3dde5a',
-    'whdfpmnzkqfkkc',
-    '41f73720d851ea0e740b9e38119c4e0261d3af5fc7eccee81545dcfb760cc47e',
+    'postgres',
+    'postgres',
+    '123456',
     {
-        host:'ec2-34-235-198-25.compute-1.amazonaws.com',
+        host:'localhost',
+        port:'5432',
         dialect:'postgres',
-        dialectOptions:{
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
     }
 );
 
